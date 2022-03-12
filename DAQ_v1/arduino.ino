7e2a1ec4fd7d276a1eae1a074c0db40d4721bc52
Sanskar Chandaliya <73470491+Suns-Caar@users.noreply.github.com>
2022-03-12 15:20:51 +0530

    Create arduino.ino

---
+++ DAQ_v1/arduino.ino
@@ -0,0 +1,160 @@
+  #include <SPI.h>
+#include <mcp2515.h>
+struct can_frame canMsg;
+struct can_frame canMsg1;
+struct can_frame canMsg2;
+struct can_frame canMsg3;
+ int i =0;
+MCP2515 mcp2515(10);
+int str[8];
+int uart;
+
+
+void setup() {
+  canMsg1.can_id  = 1365;
+  canMsg1.can_dlc = 8;
+  canMsg1.data[0] = 1;
+  canMsg1.data[1] = 2;
+  canMsg1.data[2] = 3;
+  canMsg1.data[3] = 4;
+  canMsg1.data[4] = 5;
+  canMsg1.data[5] = 6;
+  canMsg1.data[6] = 7;
+  canMsg1.data[7] = 8;
+
+  canMsg2.can_id  = 3;
+  canMsg2.can_dlc = 8;
+  canMsg2.data[0] = 6;
+  canMsg2.data[1] = 5;
+  canMsg2.data[2] = 7;
+  canMsg2.data[3] = 5;
+  canMsg2.data[4] = 2;
+  canMsg2.data[5] = 4;
+  canMsg2.data[6] = 57;
+  canMsg2.data[7] = 3;
+
+   canMsg3.can_id  = 0;
+  canMsg3.can_dlc = 8;
+  canMsg3.data[0] = 12;
+  canMsg3.data[1] = 0x00;
+  canMsg3.data[2] = 0x00;
+  canMsg3.data[3] = 12;
+  canMsg3.data[4] = 12;
+  canMsg3.data[5] = 0x00;
+  canMsg3.data[6] = 0x00;
+  canMsg3.data[7] = 11;
+  
+  
+  while (!Serial);
+  Serial.begin(115200);
+  
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_40KBPS,MCP_8MHZ);
+  mcp2515.setNormalMode();
+  
+Serial.println("Example: Write to CAN");
+Serial.println("------- CAN Read ----------");
+Serial.println("ID  DLC   DATA");
+
+   mcp2515.sendMessage(&canMsg3); 
+}
+
+void loop() {
+
+     if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
+    Serial.print(canMsg.can_id, HEX); // print ID
+    Serial.print(" "); 
+
+       for (int i = 0; i<canMsg.can_dlc; i++)  {  // print the data
+      Serial.print(canMsg.data[i],DEC);
+      Serial.print(" ");
+    }
+     
+Serial.println();      
+  }
+  if(canMsg.can_id == 4){
+    delay(10);
+     mcp2515.sendMessage(&canMsg3);
+    }
+}
+
+  #include <SPI.h>
+#include <mcp2515.h>
+struct can_frame canMsg;
+struct can_frame canMsg1;
+struct can_frame canMsg2;
+struct can_frame canMsg3;
+ int i =0;
+MCP2515 mcp2515(10);
+int str[8];
+int uart;
+
+
+void setup() {
+  canMsg1.can_id  = 1365;
+  canMsg1.can_dlc = 8;
+  canMsg1.data[0] = 1;
+  canMsg1.data[1] = 2;
+  canMsg1.data[2] = 3;
+  canMsg1.data[3] = 4;
+  canMsg1.data[4] = 5;
+  canMsg1.data[5] = 6;
+  canMsg1.data[6] = 7;
+  canMsg1.data[7] = 8;
+
+  canMsg2.can_id  = 3;
+  canMsg2.can_dlc = 8;
+  canMsg2.data[0] = 6;
+  canMsg2.data[1] = 5;
+  canMsg2.data[2] = 7;
+  canMsg2.data[3] = 5;
+  canMsg2.data[4] = 2;
+  canMsg2.data[5] = 4;
+  canMsg2.data[6] = 57;
+  canMsg2.data[7] = 3;
+
+   canMsg3.can_id  = 0;
+  canMsg3.can_dlc = 8;
+  canMsg3.data[0] = 12;
+  canMsg3.data[1] = 0x00;
+  canMsg3.data[2] = 0x00;
+  canMsg3.data[3] = 12;
+  canMsg3.data[4] = 12;
+  canMsg3.data[5] = 0x00;
+  canMsg3.data[6] = 0x00;
+  canMsg3.data[7] = 11;
+  
+  
+  while (!Serial);
+  Serial.begin(115200);
+  
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_40KBPS,MCP_8MHZ);
+  mcp2515.setNormalMode();
+  
+Serial.println("Example: Write to CAN");
+Serial.println("------- CAN Read ----------");
+Serial.println("ID  DLC   DATA");
+
+   mcp2515.sendMessage(&canMsg3); 
+}
+
+void loop() {
+
+     if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
+    Serial.print(canMsg.can_id, HEX); // print ID
+    Serial.print(" "); 
+
+       for (int i = 0; i<canMsg.can_dlc; i++)  {  // print the data
+      Serial.print(canMsg.data[i],DEC);
+      Serial.print(" ");
+    }
+     
+Serial.println();      
+  }
+  if(canMsg.can_id == 4){
+    delay(10);
+     mcp2515.sendMessage(&canMsg3);
+    }
+}
+
